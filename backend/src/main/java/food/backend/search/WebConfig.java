e86ed72576cbc67ef845bdfe1be5560b270585a2
JuHyeokParkk <hyeok5596@naver.com>
2023-08-12 10:32:40 +0900

    fix: cors 관련 이슈 해결을 위한 요청 url 목록 추가

--- backend/src/main/java/food/backend/search/WebConfig.java
+++ backend/src/main/java/food/backend/search/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080")
+                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://seektam.com", "https://seektam.com")
                 .maxAge(3000);
     }
 }
