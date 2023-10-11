ca881b442396456c6ab4906cbba5343d55247ab7
muzzleq <wngud4950@naver.com>
2023-10-11 21:47:21 +0900

    chore: allowCredentials true 옵션 추가

--- backend/src/main/java/food/backend/config/WebConfig.java
+++ backend/src/main/java/food/backend/config/WebConfig.java
@@ -25,7 +25,14 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtProvider jwtProvider;
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "http://localhost:8080", "http://www.seektam.com", "https://www.seektam.link", "https://www.seektam.com")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:8080",
+                        "http://www.seektam.com",
+                        "https://www.seektam.link",
+                        "https://www.seektam.com"
+                )
+                .allowCredentials(true)
                 .maxAge(3000);
     }
 
