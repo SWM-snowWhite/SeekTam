1d9a36ed65a5ce5b359e279d21ccc7c60f09ab73
jureamer <wngud4950@gmail.com>
2023-10-18 22:18:20 +0900

    build: @EnableJpaAuditing 추가

--- backend/src/main/java/food/backend/BackendApplication.java
+++ backend/src/main/java/food/backend/BackendApplication.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableBatchProcessing
 @SpringBootApplication
 @EnableCaching
+@EnableJpaAuditing
 public class BackendApplication {
 
 	/**
