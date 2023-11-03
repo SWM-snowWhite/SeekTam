79f7c2d433b805585a8da96ce46d077c6accdec0
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-03 20:24:13 +0900

    feat: 상품의 모든 정보를 담는 객체 생성

---
+++ backend/src/main/java/food/backend/search/dao/ProductDetailDto.java
@@ -0,0 +1,31 @@
+package food.backend.search.dao;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ProductDetailDto {
+
+    private Integer foodId;
+    private String foodCode;
+    private Integer releasedYear;
+    private String foodName;
+    private String companyName;
+    private String mainCategory;
+    private String detailedCategory;
+    private String servingSize;
+    private String servingUnit;
+    private Double totalSizeG;
+    private Double totalSizeMl;
+    private Double calorie;
+    private Double carbohydrate;
+    private Double protein;
+    private Double totalSugar;
+    private Double salt;
+    private Double fat;
+    private Double totalUnsaturatedFat;
+    private Double totalSaturatedFat;
+    private Double transFat;
+    private Double cholesterol;
+}
