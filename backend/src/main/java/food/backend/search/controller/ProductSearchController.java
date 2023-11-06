9bba9a6de7e95eb3a82dc0bd5c0cb13b2709c2ac
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-06 19:04:25 +0900

    docs: 잘못된 주석 수정 및 컨트롤러 주석 구체화

--- backend/src/main/java/food/backend/search/controller/ProductSearchController.java
+++ backend/src/main/java/food/backend/search/controller/ProductSearchController.java
@@ -25,7 +25,7 @@ public class ProductSearchController {
     private final ProductSearchService productSearchService;
 
     /**
-     * 키워드가 포함된 상품명 반환
+     * 키워드를 입력받고, 해당 키워드가 포함되는 상품명을 최대 10개 String 리스트로 반환합니다
      *
      * @param keyword : 검색 키워드
      * @return List
@@ -36,11 +36,11 @@ public class ProductSearchController {
     }
 
     /**
-     * 키워드가 포함되거나 키워드 포함 및 영양소 조건에 맞는 상품ID,상품명,제조사명 반환
+     * 키워드만 입력받거나, 키워드 및 각각의 영양소 성분함량조건을 입력받아 조건에 부합하는 상품의 상품ID,상품명,제조사명을 최대 1000개 반환합니다
      *
      * @param params : 검색 키워드, 칼로리, 칼로리 조건(0 or 1), 탄수화물, 탄수화물 조건, 단백질, 단백질 조건, 지방, 지방 조건
      * @return List
-     * @see ProductDetailDto
+     * @see ProductDto
      * @see KeywordAndNutrientEs
      */
     @GetMapping
@@ -49,7 +49,7 @@ public class ProductSearchController {
     }
 
     /**
-     * 상품의 모든 정보 반환
+     * 상품의 ID를 입력받아 해당 ID를 가지는 상품의 모든 정보(식품명,제조사명,각 영양성분 함량 등)을 반환합니다
      *
      * @param foodId : 상품 ID
      * @return ProductDetailDto
