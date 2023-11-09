7e67efc87515c1928e661c9bf2e1bcefec96fe45
동훈 <donghun@donghun-ui-MacBookPro.local>
2023-11-09 18:17:31 +0900

    fix:테스트

--- backend/src/main/java/food/backend/search/dao/FoodDetailDao.java
+++ backend/src/main/java/food/backend/search/dao/FoodDetailDao.java
@@ -96,7 +96,7 @@ public class FoodDetailDao {
                 FoodRankingResponseDto.builder()
                         .foodId(rs.getLong("food_id"))
                         .foodName(rs.getString("food_name"))
-                        .calorie(rs.getInt("calories"))
+                        .calories(rs.getInt("calories"))
                         .ranking(rs.getInt("ranking"))
                         .liked(rs.getBoolean("liked"))
                         .build();
