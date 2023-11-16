ffa618af83a90a3703335857b5cee462467cc28e
임동훈 <snowcrab382@naver.com>
2023-11-16 22:20:46 +0900

    Update deploy.sh
    Signed-off-by: 임동훈 <snowcrab382@naver.com>

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -27,4 +27,4 @@ echo ">>> 빌드 파일 경로로 이동" >> /home/ubuntu/cicd/deploy.log
 cd $BUILD_PATH
 
 echo ">>> 백그라운드 배포 시작" >> /home/ubuntu/cicd/deploy.log
-nohup java -jar $BUILD_FILE_NAME >> /home/ubuntu/deploy.log 2>/home/ubuntu/cicd/deploy_err.log &
+java -jar $BUILD_FILE_NAME >> /home/ubuntu/deploy.log 2>/home/ubuntu/cicd/deploy_err.log &
