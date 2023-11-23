bb84085888d2d92c8143a84e1600e8e3c1436701
한주형 <wngud4950@gmail.com>
2023-11-23 20:55:05 +0900

    fix: WebConfig.java
    - https://seektam.com 도메인 추가
    
    Signed-off-by: 한주형 <wngud4950@gmail.com>

--- backend/src/main/java/food/backend/config/WebConfig.java
+++ backend/src/main/java/food/backend/config/WebConfig.java
@@ -30,7 +30,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:8080",
                         "http://www.seektam.com",
                         "https://www.seektam.link",
-                        "https://www.seektam.com"
+                        "https://www.seektam.com",
+                        "https://seektam.com"
                 )
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
