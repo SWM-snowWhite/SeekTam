4303410f340644262fd38d367b3bae525159282f
한주형 <wngud4950@gmail.com>
2023-11-23 22:24:54 +0900

    fix: WebConfig.java
    - origin options 복구
    
    Signed-off-by: 한주형 <wngud4950@gmail.com>

--- backend/src/main/java/food/backend/config/WebConfig.java
+++ backend/src/main/java/food/backend/config/WebConfig.java
@@ -25,9 +25,18 @@ public class WebConfig implements WebMvcConfigurer {
     private final JwtProvider jwtProvider;
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://localhost:8080",
+                        "http://www.seektam.com",
+                        "https://www.seektam.link",
+                        "https://www.seektam.com",
+                        "https://seektam.com",
+                        "https://seektam.link"
+                )
                 .allowedHeaders("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowCredentials(true)
                 .maxAge(3600);
     }
 
